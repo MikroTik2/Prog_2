9053e2169c32eac15804abd97015bb33983a99d4
MikroTik_2 <dotsenko2034@gmail.com>
2022-10-03 14:39:15 +0300

    hamburger

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+/*------------------------------------------------------------Scroll*/
+
 const anchors = document.querySelectorAll('a[href*="#"]')
 
 for (let anchor of anchors) {
@@ -10,3 +12,28 @@ for (let anchor of anchors) {
       })
    })
 }
+
+/*------------------------------------------------------------*/
+
+const scroll = document.querySelectorAll('a[href*="#')
+
+for(let anchor of anchors) {
+   anchor.addEventListener("click", function(event) {
+      event.preventDefault();
+      const blockID = anchor.getAttribute('href')
+      document.querySelector('' + blockID).scrollIntoView({
+         behavior: "smooth",
+         block: "start"
+      })
+   })
+}
+
+/*------------------------------------------------------------Hamburger*/
+
+const burger = document.querySelector(".burger");
+const navMenu = document.querySelector(".nav-menu");
+
+burger.addEventListener("click", () => {
+   burger.classList.toggle("active");
+   navMenu.classList.toggle("active");
+})
